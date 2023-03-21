5d442b01889bfa450bddd641ea2c7f71fee3ea3c
Vazharova <maria.vazharova@sky.uk>
2023-03-21 16:55:51 +0000

    ticket 3

--- python_survey_app/script1.py
+++ python_survey_app/script1.py
@@ -7,6 +7,8 @@ def main():
     data_array = read_a_csv_file('results.csv')
     # Ticket 2
     data_deduplication = remove_duplicates(data_array)
+    # Ticket 3
+    data_without_empty_lines = ignore_empty_lines(data_deduplication)
 
 if __name__ == '__main__':
     sys.exit(main())
