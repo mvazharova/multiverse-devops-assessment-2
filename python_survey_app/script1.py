01c062c184d5a4daaf2613a1c05ce60b7db4f392
Vazharova <maria.vazharova@sky.uk>
2023-03-23 15:08:08 +0000

    ticket 6

--- python_survey_app/script1.py
+++ python_survey_app/script1.py
@@ -13,6 +13,8 @@ def main():
     data_capitalisation = capitalise_name_fields(data_without_empty_lines)
     # Ticket 5
     data_validate_ans3 = validate_ans3(data_capitalisation)
+    # Ticket 6
+    write_clean_data(data_validate_ans3, 'clean_results.csv')
 
 if __name__ == '__main__':
     sys.exit(main())
