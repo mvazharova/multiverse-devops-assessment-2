0cbbe4508daf0f9bd937904e2599dcf2ab3a66ac
mvazharova <80101207+mvazharova@users.noreply.github.com>
2023-03-29 11:51:37 +0100

    Remove get_input() as it's not used

--- python_survey_app/csvhelper/__init__.py
+++ python_survey_app/csvhelper/__init__.py
@@ -1,15 +1,5 @@
 import csv
 
-def get_input(filename):
-    data = []
-    # f =  open(filename) # risky as we need to close it
-    with open(filename) as f: # open(filename, 'r') r stands for read, w stands for write, a for append and could do a combination
-        for line in f.readlines():
-            # clean_line = line.strip()
-            # line_list = clean_line.split(',')
-            # data.append(line_list)
-            data.append(line.strip().split(','))
-
 # Ticket 1
 def read_a_csv_file(filename):
     if not filename.endswith('.csv'):
